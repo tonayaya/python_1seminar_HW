38f73844b65fbbf3241c855eb3f93ebc8904ca17
tonayaya <tonayaya@icloud.com>
2022-12-22 21:34:59 +0400

    ex1 hw3

--- seminar3/ex1.py
+++ seminar3/ex1.py
@@ -2,3 +2,15 @@
 # Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+in_list = list(input('Задайте список из нескольких чисел без разделителей: '))
+print(in_list)
+n = len(in_list)
+summ_chet = 0
+for i in range(0, n, 2):
+    summ_chet += int(in_list[i])
+summ_nechet = 0
+for i in range(1, n, 2):
+    summ_nechet += int(in_list[i])
+print('Сумма чётных элементов: ', (summ_chet))
+print('Сумма НЕчётных элементов: ', + (summ_nechet))
