a755b9950ed75a9553c5085ad044bd625a01425c
tonayaya <tonayaya@icloud.com>
2022-12-22 20:14:08 +0400

    ex1 готова

--- seminar2/ex1.py
+++ seminar2/ex1.py
@@ -2,3 +2,21 @@
 # которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def number():
+    try: 
+        num = int(input('enter N: '))
+        return abs(num)   #на случай ввода отрицательного числа
+    except ValueError:
+        print('Incorrect input!')
+        return number()
+
+num_N = number()
+
+list_factorial = []
+num_list = 1
+for i in range(1, num_N + 1): #(1*1, 1*2, 2*3, 6*4, 24*...)
+    num_list *= i
+    list_factorial.append(num_list)
+
+print('N =', num_N,' → ', list_factorial)
