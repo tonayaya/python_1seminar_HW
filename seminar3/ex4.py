50a3b34d8774da7de4dc7592bedef632332c406e
tonayaya <tonayaya@icloud.com>
2022-12-22 21:43:42 +0400

    ex4 hw3

--- seminar3/ex4.py
+++ seminar3/ex4.py
@@ -3,3 +3,14 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+n = int(input('Введите деятичное число: '))
+n_out = n # используем для красивой печати
+binary = []
+x = n
+while x >= 1:
+    x = n // 2
+    binary.append(n - x * 2)
+    n = x
+else:
+    print(f'{n_out} -> ', *binary, sep='')
