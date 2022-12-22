718438770772cfc5df0d53d51cc64ff8a6909451
tonayaya <tonayaya@icloud.com>
2022-12-22 21:41:53 +0400

    ex3 hw3

--- seminar3/ex3.py
+++ seminar3/ex3.py
@@ -2,3 +2,20 @@
 # Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+n = int(input('Сколько вещественных чисел вы будете задаваь? '))
+if n < 2:
+    print('Надо ввести минимум 2 числа!')
+    exit()
+in_list = list([0] * n)
+for _ in range(n):
+    in_list[_] = float(input('Введите число: '))
+print(in_list)
+maxx = in_list[0] % 1
+mini_maxx = in_list[1] % 1
+for i in range(n):
+    if in_list[i] % 1 !=0 and in_list[i] % 1 >= maxx:
+        maxx = in_list[i] % 1
+    if in_list[i] % 1 !=0 and in_list[i] % 1 <= mini_maxx:
+        mini_maxx = in_list[i] % 1 
+print(round((maxx - mini_maxx), 2))
