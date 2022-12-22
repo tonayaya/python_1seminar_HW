625a1a28ddff501c22aad4f68722816a240c352a
tonayaya <tonayaya@icloud.com>
2022-12-22 20:28:42 +0400

    ex3 HW2

--- seminar2/ex3.py
+++ seminar2/ex3.py
@@ -3,3 +3,19 @@
 # Пять ИНДЕКСОВ хранятся в списке, который вы сами заполняете.
 # Пример списка ИНДЕКСОВ [2, 2, 3, 1, 8]
 # Ввод: 4 [-4, -3, -2, -1, 0, 1, 2, 3,4]
+
+from random import randint
+F = 5 # const
+n = int(input('n = '))
+newList = []
+
+for i in range(-n, n+1):
+    newList.append(i)
+print(newList)
+nList = []
+composition = 1
+
+for i in range(F + 1):
+    nList.append(randint(0, len(newList)))
+composition *= nList[i]
+print(f'произведение элементов на индексах: {nList} равно {composition}')
