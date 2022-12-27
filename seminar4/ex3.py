2e3b9bd8f881c07e8a384b51a7719e839fe59aa7
tonayaya <tonayaya@icloud.com>
2022-12-27 19:34:40 +0400

    ex3_HW4

--- seminar4/ex3.py
+++ seminar4/ex3.py
@@ -5,3 +5,14 @@
 # Вывод:
 # 2 1
 
+lst = []
+lst_2 = []
+num = int(input("Cколько цифр хотите внести в список: "))
+for i in range(num):
+    ind = int(input(f"Введите {i+1} число: "))
+    lst.append(ind)
+for j in lst:
+    if lst.count(j) < 2:
+        lst_2.append(j)
+print(f"Список вводимых чисел: {lst}")
+print(f"Список неповторяющихся элементов вводимых чисел: {lst_2}")
