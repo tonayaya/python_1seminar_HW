c7de651f7a698d7f3e574fbcfd9703dc6ae6c885
tonayaya <tonayaya@icloud.com>
2023-02-02 18:26:31 +0400

    seminar 10 HW bot-calculator

--- seminar10/main.py
+++ seminar10/main.py
@@ -2,35 +2,36 @@
 # Создать калькулятор для работы с рациональными, организовать меню, добавив в неё систему логирования
 # (Содержит: id.Пользователь, ввод, результат)
 
+
 from cmath import log
 from email.message import Message
 from fractions import Fraction
 import telebot
 
+
 first=""
 second=""
 history_for_bot = open('history_for_bot .txt','r+', encoding='utf-8')
-
 bot = telebot.TeleBot('5460917203:AAFcw5YBaPs57Q9f6elHGP1HRVkjbaus1No')
-
 del_buttons = telebot.types.ReplyKeyboardRemove()
-
 buttons1 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons1.row(telebot.types.KeyboardButton('Комплексные'),
             telebot.types.KeyboardButton('Рациональные'),)
 buttons1.row(telebot.types.KeyboardButton('Ещё не определился'))
-
 buttons2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons2.row(telebot.types.KeyboardButton('+'),
             telebot.types.KeyboardButton('-'))
 buttons2.row(telebot.types.KeyboardButton('*'),
             telebot.types.KeyboardButton('/'))
 
+
+
 def message(chat_id, text, reply_markup = None):
     bot.send_message(chat_id=chat_id,
                 text=text,
                 reply_markup=reply_markup)
     history_for_bot.write("БОТ:" + str(text) + '\n')
+
 
 
 @bot.message_handler(commands=['log'])
@@ -43,12 +44,14 @@ def hello(msg: telebot.types.Message):
                     reply_markup=del_buttons)
 
 
+
 @bot.message_handler()
 def hello(msg: telebot.types.Message):
     message(chat_id=msg.from_user.id,
                     text='Здравствуйте.\nВыберите режим работы калькулятора.',
                     reply_markup=buttons1)
     bot.register_next_step_handler(msg, answer)
+
 
 
 def answer(msg: telebot.types.Message):
@@ -72,6 +75,7 @@ def answer(msg: telebot.types.Message):
         message(chat_id=msg.from_user.id, text='Выберите режим работы калькулятора.', reply_markup=buttons1)
 
 
+
 def second_msg(msg: telebot.types.Message):
     history_for_bot.write("USER:" + msg.text + '\n')
     global first
@@ -85,6 +89,7 @@ def second_msg(msg: telebot.types.Message):
                         reply_markup=del_buttons)
 
 
+
 def sign(msg: telebot.types.Message):
     history_for_bot.write("USER:" + msg.text + '\n')
     global second
@@ -94,6 +99,7 @@ def sign(msg: telebot.types.Message):
         second=Fraction(int(msg.text.split('/')[0]), int(msg.text.split('/')[1]))
     bot.register_next_step_handler(msg,counter)
     message(chat_id=msg.from_user.id,text='Введите знак', reply_markup=buttons2)
+
 
 
 def counter(msg: telebot.types.Message):
@@ -107,6 +113,8 @@ def counter(msg: telebot.types.Message):
         message(chat_id=msg.from_user.id, text=first * second)
     if msg.text == "/":
         message(chat_id=msg.from_user.id, text=first / second)
+    
+
     
 bot.polling()    
 history_for_bot.close()
